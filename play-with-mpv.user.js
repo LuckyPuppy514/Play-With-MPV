f8c0a4cc7fb3f0663675c06c1cb84a2419689c33
LuckyPuppy514 <122154133@qq.com>
2023-10-24 19:06:42 +0800

    v3.9.6 合并来自 orz12 的 PR: #120

--- play-with-mpv.user.js
+++ play-with-mpv.user.js
@@ -2,7 +2,7 @@
 // @name                    Play-With-MPV
 // @name:zh                 使用 MPV 播放
 // @namespace               https://github.com/LuckyPuppy514
-// @version                 3.9.5
+// @version                 3.9.6
 // @author                  LuckyPuppy514
 // @copyright               2023, Grant LuckyPuppy514 (https://github.com/LuckyPuppy514)
 // @license                 MIT
